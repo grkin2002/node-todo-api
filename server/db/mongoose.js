1f30083a292bcdaa2d727020b7f9ad9f44d32c34
greg pong <grkin2002@gmail.com>
2018-09-15 12:43:59 +1000

    Set up app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-const db = mongoose.connect('mongodb://localhost:27017/TodoApp',{
-    useMongoClient: true,
-    keepAlive: false
-  });
+// const db = mongoose.connect('mongodb://localhost:27017/TodoApp', {useMongoClient: true});
+const db = mongoose.connect(
+  'mongodb://greg:greg123@ds257752.mlab.com:57752/todo-app-api', { useMongoClient: true });
 
-module.exports = {mongoose};
+module.exports = { mongoose };
